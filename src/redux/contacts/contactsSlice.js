4a5d3d2fcbf5c5381f9cfefcfdb39445a28a2fff
Fluebubble <Trots.anatoly@gmail.com>
2023-02-14 12:39:39 +0200

    Update contactsSlice.js

--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -19,42 +19,7 @@ const contactsSlice = createSlice({
       .addCase(getContactsList.fulfilled, (state, action) => {
         state.items = action.payload;
       }),
-  // {
-  //   [fetchContacts.pending]: state => {
-  //     state.isLoading = true;
-  //   },
-  //   [fetchContacts.fulfilled]: (state, action) => {
-  //     state.isLoading = false;
-  //     state.items = action.payload;
-  //     state.error = null;
-  //   },
-  //   [fetchContacts.rejected]: (state, action) => {
-  //     state.isLoading = false;
-  //     state.error = action.payload;
-  //   },
-  //   [postContact.pending]: state => {
-  //     state.isLoading = true;
-  //   },
-  //   [postContact.fulfilled]: state => {
-  //     state.isLoading = false;
-  //     state.error = null;
-  //   },
-  //   [postContact.rejected]: (state, action) => {
-  //     state.isLoading = false;
-  //     state.error = action.payload;
-  //   },
-  //   [deleteContact.pending]: state => {
-  //     state.isLoading = true;
-  //   },
-  //   [deleteContact.fulfilled]: state => {
-  //     state.isLoading = false;
-  //     state.error = null;
-  //   },
-  //   [deleteContact.rejected]: (state, action) => {
-  //     state.isLoading = false;
-  //     state.error = action.payload;
-  //   },
-  // },
+
 });
 
 export const contactsReducer = contactsSlice.reducer;
